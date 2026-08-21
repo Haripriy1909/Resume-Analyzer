6e3a64cd0c5c30a1a43b1320caf2a2b0fdc33dc4
Darji Haripriy <haripriydarji1909@gmail.com>
2026-08-21 14:03:02 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,5 @@
-const API_BASE = (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1")
-  ? "http://127.0.0.1:5000/api"
+const API_BASE = (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1" || window.location.hostname.startsWith("192.168."))
+  ? `http://${window.location.hostname}:5000/api`
   : "/api";
 
 const hamburgerBtn = document.getElementById("hamburgerBtn");
